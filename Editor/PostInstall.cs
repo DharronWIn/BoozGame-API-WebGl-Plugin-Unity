5860ae48a4d2963f89adb34779b57b4510e47082
Lionel Abrogoua <95973826+DharronWIn@users.noreply.github.com>
2025-05-07 16:39:08 +0000

    post install

--- Editor/PostInstall.cs
+++ Editor/PostInstall.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 using System.IO;
 
 [InitializeOnLoad]
-public static class PostInstall
+public static class BoozClientPostInstall
 {
-    static PostInstall()
+    static BoozClientPostInstall()
     {
-        string sourcePath = "Packages/com.boozgame.webglclient/RunTime";
+        string packagePath = "Packages/com.boozgame.webglclient";
         string targetPath = "Assets/BoozClient";
 
         if (!Directory.Exists(targetPath))
         {
-            FileUtil.CopyFileOrDirectory(sourcePath, targetPath);
+            Debug.Log("[BoozClient] Installation détectée. Copie des fichiers dans Assets/BoozClient...");
+            FileUtil.CopyFileOrDirectory(packagePath, targetPath);
             AssetDatabase.Refresh();
-            Debug.Log("[BoozClient] Prefabs copied to Assets/BoozClient");
+            Debug.Log("[BoozClient] Fichiers copiés avec succès !");
+        }
+        else
+        {
+            Debug.Log("[BoozClient] Le dossier Assets/BoozClient existe déjà. Aucune action.");
         }
     }
 }
